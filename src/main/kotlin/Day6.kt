9440e4d439c316f2d43d08d1179e493aa1f7eb78
Sebastian Muskalla <SebastianMuskalla@users.noreply.github.com>
2023-12-06 01:58:28 +0100

    Cleanup

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,14 @@
+private fun partOne(lines : List<String>) : Long {
+    return 0L
+}
+
+private fun partTwo(lines : List<String>) : Long {
+    return 0L
+}
+
+private fun main() {
+    aoc("06-example.txt", 0L) { partOne(it) }
+//    aoc("06-input.txt", null) { partOne(it) }
+//    aoc("06-example.txt", null) { partTwo(it) }
+//    aoc("06-input.txt", null) { partTwo(it) }
+}
---
+++ src/main/kotlin/Util.kt
@@ -0,0 +1,29 @@
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+import kotlin.time.measureTimedValue
+
+fun aoc(fileName : String, check : Any? = null, fn : (List<String>) -> Any?) {
+    val lines = Path("src/main/resources/$fileName")
+        .readLines()
+
+    val timedSolution = measureTimedValue { fn(lines) }
+
+    println(
+        """
+            
+            ==========================
+            ${timedSolution.value}
+            computed in ${timedSolution.duration}
+            ==========================
+            
+        """.trimIndent()
+    )
+
+    if (check != null && timedSolution.value != check) {
+        throw AssertionError("Computed solution ${timedSolution.value} does not coincide with expected solution $check")
+    }
+
+}
+
+const val debug = true
+fun log(message : String) = if (debug) println("DEBUG: $message") else Unit
